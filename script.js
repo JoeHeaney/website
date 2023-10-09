6c08222d7529a7e7170c7ec7edddadee8fe9adfb
JoeHeaney <{ID}+{username}@users.noreply.github.com>
2023-10-09 16:48:22 -0400

    Updated-October-2023

---
+++ script.js
@@ -0,0 +1,6 @@
+const typed = new Typed(".animate", {
+    strings: ["graduate student.", "coder.", "game enjoyer.", "NFL fan.", "future Software Engineer."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+  });
